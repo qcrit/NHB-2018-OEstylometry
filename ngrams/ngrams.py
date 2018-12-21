8708bd38abaf12cf9e15d5d815dc3eec8d56d72d
Joseph <jdexter@fas.harvard.edu>
2018-12-20 23:52:51 -0500

    Adding running sum for ngrams

--- ngrams/ngrams.py
+++ ngrams/ngrams.py
@@ -4,7 +4,7 @@ import tools as ts
 def NG_main(mydata, savefile, N, X):	# given array of texts, place to save, type of ngram, number of ngrams per text
 															# perform analysis of each text
 	myfile = open(savefile,'wb')
-	myfile.write("name,ngram,textfreq,corpfreq,diff,ngram,etc\n".encode('utf-8'))
+	myfile.write("name,ngram,textfreq,corpfreq,diff,diffsum,length, ngram,etc\n".encode('utf-8'))
 	
 	# useful data
 	corpStrSpaces = ts.generateCorpstr(mydata,spaces=True)
@@ -21,7 +21,8 @@ def NG_main(mydata, savefile, N, X):	# given array of texts, place to save, type
 		commongrams = ngramDist.most_common(X)
 
 		writestuff = datapiece[0]+"," # write the name of text
-		
+
+		diffSum = 0
 		for fgram in commongrams:
 			writestuff += "".join(fgram[0]) + "," # write the ngram being counted
 			
@@ -31,10 +32,15 @@ def NG_main(mydata, savefile, N, X):	# given array of texts, place to save, type
 			else:
 				textFreq =  fgram[1]/len(datapiece[1])
 				corpFreq = corpStrNoSpaces.count(  "".join(fgram[0])  )/len(corpStrSpaces)
+				diff = abs(textFreq-corpFreq)
+				diffSum += abs(textFreq-corpFreq)
+				length = len(datapiece[1])
 			 
 			writestuff += str(  round(textFreq,8)) + ","
 			writestuff += str(  round(corpFreq,8)) + ","
-			writestuff += str(  round(abs(textFreq-corpFreq),8)) + ","
+			writestuff += str(  round(diff,8)) + ","
+			writestuff += str(	round(diffSum, 8)) + ","
+			writestuff += str(  round(length,8)) + ","
 
 		writestuff += '\n'
 		myfile.write(writestuff.encode('utf-8'))
